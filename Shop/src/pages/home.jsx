84d351275faed3b1bee3166d76123d168b6e3e8b
Vlad Beltran <puentes.studio@gmail.com>
2024-04-16 16:55:39 +0100

    products - fetch is not working with the api

--- Shop/src/pages/home.jsx
+++ Shop/src/pages/home.jsx
@@ -1,3 +1,38 @@
+import { useEffect, useState } from "react";
+import { ColorRing } from "react-loader-spinner";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchListOfProducts() {
+    setLoading(true);
+    const res = await fetch("https://fakestore.com/api/products");
+    const data = await res.json();
+
+    if (data) {
+      setLoading(false);
+      setProducts(data);
+      Products(data);
+    }
+  }
+
+  useEffect(() => {
+    fetchListOfProducts();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <div className="min-h-screen w-full flex justify-center items-center">
+          <ColorRing
+            height={"120"}
+            width={"120"}
+            color={"rgba(255,255,255,"}
+            visible={true}
+          />
+        </div>
+      ) : null}
+    </div>
+  );
 }
