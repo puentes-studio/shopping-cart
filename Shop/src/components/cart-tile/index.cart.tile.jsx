efe3f897542017a27d6f11544f1a9343a6365fbd
Vlad Beltran <puentes.studio@gmail.com>
2024-04-18 15:59:21 +0100

    shop and cart working, need styles

---
+++ Shop/src/components/cart-tile/index.cart.tile.jsx
@@ -0,0 +1,36 @@
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../../store/slices/cart.slice";
+
+export default function CartTile({ cartItem }) {
+  const dispatch = useDispatch();
+
+  function handleRemoveFromCart() {
+    dispatch(removeFromCart(cartItem.id));
+  }
+
+  return (
+    <div>
+      <div className="flex items-center p-5 justify-between bg-gray-200 mt-2 mb-2 rounded-xl w-96">
+        <div className="flex p-3 ">
+          <img
+            className="h-28 rounded-lg"
+            src={cartItem?.image}
+            alt={cartItem?.title}
+          />
+          <div className="ml-10 self-start space-y-5">
+            <h1 className="text-m text-gray-700 font-bold">
+              {cartItem?.title}
+            </h1>
+            <p className="text-gray-700 font-extrabold">$ {cartItem?.price}</p>
+            <button
+              onClick={handleRemoveFromCart}
+              className="text-gray-500 text-xs mt-1"
+            >
+              Remove from cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
